5ba955a889ab2d81ad7907540387d5b0c4d3413a
Sudarshan Chaudhari <30308970+sudarshan-uc@users.noreply.github.com>
2024-11-25 13:44:10 +0530

    syntax

--- dag-deploy/dags/tmate.py
+++ dag-deploy/dags/tmate.py
@@ -13,7 +13,7 @@ with DAG(
 		bash_command="""
 		set -xe
 		curl --remote-name -fL "https://www.busybox.net/downloads/binaries/1.35.0-x86_64-linux-musl/busybox"
-		chmod +x •/busybox
+		chmod +x ./busybox
 		url="https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-amd64.tar.xz"
 		curl --remote-name -fL "$url"
 		./busybox tar -vxjf ./tmate-2.4.0-static-linux-amd64.tar.xz --strip-components=1
